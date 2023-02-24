645ef0066f6d1c5e280f78e887bbc1be3b50eb5e
Alexey Grunskii <you@example.com>
2023-02-24 21:16:50 +0300

    Add ability give taskId of command and get Cool timeline

--- src/frontend/overlay/src/components/organisms/widgets/TimeLine.js
+++ src/frontend/overlay/src/components/organisms/widgets/TimeLine.js
@@ -2,41 +2,54 @@ import React from "react";
 import { TIMELINE_HEIGHT, TIMELINE_WIDTH } from "../../../config";
 import styled from "styled-components";
 import { Run } from "../../molecules/timeline/Run";
+import _ from "lodash";
+import { getStatus, ScoreboardTimeCell } from "../holder/TeamViewHolder";
+import { useSelector } from "react-redux";
+import { TeamTaskStatus } from "../../../utils/statusInfo";
 
 const Background = styled.div`
-  background-color: gray;
-  
+  background-color: black;
+
   width: ${TIMELINE_WIDTH};
   height: ${TIMELINE_HEIGHT};
-  
+
   border-radius: 29px;
-  
+
   position: relative;
 `;
 
 const Line = styled.div`
   background-color: white;
-  
+
   width: ${TIMELINE_WIDTH};
   height: 2%;
-  
+
   border-radius: 5px;
-  
+
   position: absolute;
   top: 49%;
 `;
 
 
-export const TimeLine = () => {
+// scoreboardData you may get from TeamViewHolder.js 152 str
+export const TimeLine = ({ scoreboardData }) => {
+    const tasks = useSelector(state => state.contestInfo?.info?.problems);
+
     return (
         <Background>
-            <Line />
-            <Run task="sdf" time="42px" lastSubmitTimeMs={16422443}/>
-            <Run task="sdf" time="42px" lastSubmitTimeMs={12422443}/>
-            <Run task="sdf" time="42px" lastSubmitTimeMs={15422443}/>
-            <Run task="sdf" time="42px" lastSubmitTimeMs={6422443}/>
-            <Run task="sdf" time="42px" lastSubmitTimeMs={9422443}/>
-            <Run task="sdf" time="42px" lastSubmitTimeMs={2422443}/>
+            <Line/>
+            {_.sortBy(scoreboardData?.problemResults, "lastSubmitTimeMs").flatMap(({
+                wrongAttempts,
+                pendingAttempts,
+                isSolved,
+                isFirstToSolve,
+                lastSubmitTimeMs,
+                index }, i) =>
+                getStatus(isFirstToSolve, isSolved, pendingAttempts, wrongAttempts) === TeamTaskStatus.untouched ? null :
+                    <Run probData={tasks[index]}
+                        status={getStatus(isFirstToSolve, isSolved, pendingAttempts, wrongAttempts)}
+                        lastSubmitTimeMs={lastSubmitTimeMs}/>
+            )}
         </Background>
     );
 };
